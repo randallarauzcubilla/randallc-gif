1cc0ee3a2e8bd290af718c3099cdfd1a30934263
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-10-24 08:41:50 -0600

    First commit

---
+++ randallC/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"randallC")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"HP")];
+[assembly:AssemblyProductAttribute(L"randallC")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c) HP 2025")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute(L"1.0.*")];
+
+[assembly:ComVisible(false)];
