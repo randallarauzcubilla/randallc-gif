e0983b8ae3971e231e5613728e8997dfbec8cdae
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-10-24 09:39:29 -0600

    Implement ArbolUsuarios class with BST logic: insert, search, delete, and traversal methods.

---
+++ randallC/ArbolUsuarios.cpp
@@ -0,0 +1,126 @@
+﻿#include "pch.h"
+#include "ArbolUsuarios.h"
+#include <map>
+
+ArbolUsuarios::ArbolUsuarios() : raiz(nullptr) {}
+
+void ArbolUsuarios::insertar(Usuario*& nodo, Usuario* nuevo) {
+    if (!nodo) {
+        nodo = nuevo;
+    }
+    else if (nuevo->id < nodo->id) {
+        insertar(nodo->izq, nuevo);
+    }
+    else {
+        insertar(nodo->der, nuevo);
+    }
+}
+
+void ArbolUsuarios::insertarUsuario(int id, std::string nombre, int edad, float saldo) {
+    Usuario* nuevo = new Usuario(id, nombre, edad, saldo);
+    insertar(raiz, nuevo);
+}
+
+Usuario* ArbolUsuarios::buscar(Usuario* nodo, int id) {
+    if (!nodo || nodo->id == id) return nodo;
+    return (id < nodo->id) ? buscar(nodo->izq, id) : buscar(nodo->der, id);
+}
+
+Usuario* ArbolUsuarios::buscarUsuario(int id) {
+    return buscar(raiz, id);
+}
+
+Usuario* ArbolUsuarios::encontrarMinimo(Usuario* nodo) {
+    while (nodo && nodo->izq) nodo = nodo->izq;
+    return nodo;
+}
+
+Usuario* ArbolUsuarios::eliminar(Usuario* nodo, int id) {
+    if (!nodo) return nullptr;
+    if (id < nodo->id) {
+        nodo->izq = eliminar(nodo->izq, id);
+    }
+    else if (id > nodo->id) {
+        nodo->der = eliminar(nodo->der, id);
+    }
+    else {
+        if (!nodo->izq) {
+            Usuario* temp = nodo->der;
+            delete nodo;
+            return temp;
+        }
+        else if (!nodo->der) {
+            Usuario* temp = nodo->izq;
+            delete nodo;
+            return temp;
+        }
+        else {
+            Usuario* temp = encontrarMinimo(nodo->der);
+            nodo->id = temp->id;
+            nodo->nombre = temp->nombre;
+            nodo->edad = temp->edad;
+            nodo->saldo = temp->saldo;
+            nodo->der = eliminar(nodo->der, temp->id);
+        }
+    }
+    return nodo;
+}
+
+void ArbolUsuarios::eliminarUsuario(int id) {
+    raiz = eliminar(raiz, id);
+}
+
+void ArbolUsuarios::recorrerInOrden(Usuario* nodo, std::function<void(Usuario*)> f) {
+    if (nodo) {
+        recorrerInOrden(nodo->izq, f);
+        f(nodo);
+        recorrerInOrden(nodo->der, f);
+    }
+}
+
+void ArbolUsuarios::recorrerPreOrden(Usuario* nodo, std::function<void(Usuario*)> f) {
+    if (nodo) {
+        f(nodo);
+        recorrerPreOrden(nodo->izq, f);
+        recorrerPreOrden(nodo->der, f);
+    }
+}
+
+void ArbolUsuarios::recorrerPostOrden(Usuario* nodo, std::function<void(Usuario*)> f) {
+    if (nodo) {
+        recorrerPostOrden(nodo->izq, f);
+        recorrerPostOrden(nodo->der, f);
+        f(nodo);
+    }
+}
+
+void ArbolUsuarios::mostrarInOrden() {
+    recorrerInOrden(raiz, [](Usuario* u) {
+        std::cout << u->id << " - " << u->nombre << " - " << u->edad << " anios " << u->saldo << " colones.\n";
+        });
+}
+
+void ArbolUsuarios::mostrarPreOrden() {
+    recorrerPreOrden(raiz, [](Usuario* u) {
+        std::cout << u->id << " - " << u->nombre << "\n";
+        });
+}
+
+void ArbolUsuarios::mostrarPostOrden() {
+    recorrerPostOrden(raiz, [](Usuario* u) {
+        std::cout << u->id << " - " << u->nombre << "\n";
+        });
+}
+
+void ArbolUsuarios::mostrarOrdenadosPorNombre() {
+    std::map<std::string, Usuario*> mapa;
+    recorrerInOrden(raiz, [&](Usuario* u) {
+        mapa[u->nombre] = u;
+        });
+
+    std::cout << "\nUsuarios ordenados por nombre:\n";
+    for (auto& par : mapa) {
+        Usuario* u = par.second;
+        std::cout << u->id << " - " << u->nombre << " - " << u->edad << " anios - saldo: " << u->saldo << " colones.\n";
+    }
+}
---
+++ randallC/ArbolUsuarios.h
@@ -0,0 +1,31 @@
+#ifndef ARBOL_USUARIOS_H
+#define ARBOL_USUARIOS_H
+
+#include "Usuario.h"
+#include <iostream>
+#include <functional>
+
+class ArbolUsuarios {
+private:
+    Usuario* raiz;
+
+    void insertar(Usuario*& nodo, Usuario* nuevo);
+    Usuario* buscar(Usuario* nodo, int id);
+    Usuario* eliminar(Usuario* nodo, int id);
+    Usuario* encontrarMinimo(Usuario* nodo);
+    void recorrerInOrden(Usuario* nodo, std::function<void(Usuario*)> f);
+    void recorrerPreOrden(Usuario* nodo, std::function<void(Usuario*)> f);
+    void recorrerPostOrden(Usuario* nodo, std::function<void(Usuario*)> f);
+
+public:
+    ArbolUsuarios();
+    void insertarUsuario(int id, std::string nombre, int edad, float saldo);
+    Usuario* buscarUsuario(int id);
+    void eliminarUsuario(int id);
+    void mostrarInOrden();
+    void mostrarPreOrden();
+    void mostrarPostOrden();
+    void mostrarOrdenadosPorNombre();
+};
+
+#endif
