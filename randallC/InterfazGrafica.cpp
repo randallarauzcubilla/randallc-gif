5cbe7fe2e62ca697b8e4ac8b4e3b088e3e362d43
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-10-30 22:25:20 -0600

    Refactor InterfazGrafica.cpp: improve navigation flow and clean up form lifecycle handling

---
+++ randallC/InterfazGrafica.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "InterfazGrafica.h"
+#include "Usuarios.h"
+#include "RUTAS.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    Application::Run(gcnew randallC::InterfazGrafica());
+    return 0;
+}
+
+System::Void randallC::InterfazGrafica::btnMenuUsuarios_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Hide();
+	randallC::Usuarios^ usuarios = gcnew randallC::Usuarios();
+	usuarios->ShowDialog();
+	this->Show();
+}
+
+System::Void randallC::InterfazGrafica::btnMenuRutas_Click(System::Object^ sender, System::EventArgs^ e) {
+	this->Hide();
+	randallC::Rutas^ rutas = gcnew randallC::Rutas();
+	rutas->ShowDialog();
+	this->Show();
+}
