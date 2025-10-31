5bf351125cee1f9e3080ca3536a385b852352dd4
Randall Arauz Cubilla <randall.arauz.cubilla@est.una.ac.cr>
2025-10-30 22:20:23 -0600

    Update ArbolUsuarios.h: declare new traversal and export methods

--- randallC/ArbolUsuarios.h
+++ randallC/ArbolUsuarios.h
@@ -4,6 +4,8 @@
 #include "Usuario.h"
 #include <iostream>
 #include <functional>
+#include <string>
+#include <map>
 
 class ArbolUsuarios {
 private:
@@ -13,22 +15,30 @@ private:
     Usuario* buscar(Usuario* nodo, int id);
     Usuario* eliminar(Usuario* nodo, int id);
     Usuario* encontrarMinimo(Usuario* nodo);
-    void recorrerInOrden(Usuario* nodo, std::function<void(Usuario*)> f);
-    void recorrerPreOrden(Usuario* nodo, std::function<void(Usuario*)> f);
-    void recorrerPostOrden(Usuario* nodo, std::function<void(Usuario*)> f);
+
+    void recorrerInOrden(Usuario* nodo, std::function<void(Usuario*)> f) const;
+    void recorrerPreOrden(Usuario* nodo, std::function<void(Usuario*)> f) const;
+    void recorrerPostOrden(Usuario* nodo, std::function<void(Usuario*)> f) const;
 
 public:
     ArbolUsuarios();
     void insertarUsuario(int id, std::string nombre, int edad, float saldo);
     Usuario* buscarUsuario(int id);
     void eliminarUsuario(int id);
+
     void mostrarInOrden();
     void mostrarPreOrden();
     void mostrarPostOrden();
     void mostrarOrdenadosPorNombre();
+
     void guardarEnArchivo(const std::string& nombreArchivo);
     void cargarDesdeArchivo(const std::string& nombreArchivo);
     void exportarOrdenadosPorNombre(std::ostream& out);
+
+    std::string obtenerRecorridoInOrden() const;
+    std::string obtenerRecorridoPreOrden() const;
+    std::string obtenerRecorridoPostOrden() const;
+    std::string obtenerUsuariosOrdenadosPorNombre() const;
 };
 
 #endif
